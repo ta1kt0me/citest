7f3bad353b89bea378ea38bda48af3fe68cc2a37
ta1kt0me <p.wadachi@gmail.com>
2020-02-09 15:05:31 +0900

    Include username in image name

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=practice .
+docker build --tag=talktome/practice .
 
 # Step 2:
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3:
 # Run flask app
-docker run -p 8000:80 practice
+docker run -p 8000:80 talktome/practice
