b49b6289256253d8f204b370af33507e75dc217a
ta1kt0me <p.wadachi@gmail.com>
2020-02-09 16:05:08 +0900

    Setup a kubernetes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="talktome/practice:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --generator=run-pod/v1 --image=$dockerpath practice
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/practice 8000:80
